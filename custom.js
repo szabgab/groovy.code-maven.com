54790daa19b86304dbf4f8c322c26fb0a67da8a4
Gabor Szabo <gabor@szabgab.com>
2026-05-13 15:05:17 +0300

    url

--- custom.js
+++ custom.js
@@ -1,4 +1,4 @@
 const prefixes = ["src/examples/", "examples/"];
 const extensions = [".py", ".toml", ".cfg", ".csv", ".txt", ".gvy", ".groovy"];
-const base_url = "https://github.com/szabgab/exercises.code-maven.com/tree/main/"
+const base_url = "https://github.com/szabgab/groovy.code-maven.com/tree/main/"
 
